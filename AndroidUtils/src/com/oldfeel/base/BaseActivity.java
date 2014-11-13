c852f395a128fc5454549e22c6c2bd23edf789ff
oldfeel <hyt5926@163.com>
2014-11-13 16:30:21 +0800

    123456

--- AndroidUtils/src/com/oldfeel/base/BaseActivity.java
+++ AndroidUtils/src/com/oldfeel/base/BaseActivity.java
@@ -232,4 +232,9 @@ public abstract class BaseActivity extends SwipeBackActivity {
 		super.startActivity(intent);
 		overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+	}
 }
