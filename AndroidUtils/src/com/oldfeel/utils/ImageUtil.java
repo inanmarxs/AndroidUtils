64d2760c3791e524349b9ce92afa31b6f0539da5
oldfeel <hyt5926@163.com>
2014-11-16 12:45:34 +0800

    添加压缩图片和旋转图片的方法.

--- AndroidUtils/src/com/oldfeel/utils/ImageUtil.java
+++ AndroidUtils/src/com/oldfeel/utils/ImageUtil.java
@@ -1,6 +1,7 @@
 package com.oldfeel.utils;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -30,6 +31,7 @@ import android.graphics.RectF;
 import android.graphics.Shader.TileMode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.ExifInterface;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Environment;
@@ -79,6 +81,23 @@ public class ImageUtil {
 		byte[] bytes = stream.toByteArray();
 		fos.write(bytes);
 		fos.close();
+	}
+
+	/**
+	 * 压缩并保存图片
+	 * 
+	 * @param context
+	 * @param filePath
+	 * @param bitmap
+	 */
+	public static void compAndSaveImage(Context context, String filePath,
+			Bitmap bitmap) {
+		bitmap = comp(bitmap);
+		try {
+			saveImageToSD(context, filePath, bitmap, 100);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -748,5 +767,113 @@ public class ImageUtil {
 				.getColumnIndex(MediaStore.Images.Media.DATA));
 		cursor.close();
 		return path;
+	}
+
+	/**
+	 * 图片按比例大小压缩方法（根据Bitmap图片压缩）
+	 * 
+	 * @param image
+	 * @return
+	 */
+	public static Bitmap comp(Bitmap image) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+		if (baos.toByteArray().length / 1024 > 1024) {// 判断如果图片大于1M,进行压缩避免在生成图片（BitmapFactory.decodeStream）时溢出
+			baos.reset();// 重置baos即清空baos
+			image.compress(Bitmap.CompressFormat.JPEG, 50, baos);// 这里压缩50%，把压缩后的数据存放到baos中
+		}
+		ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
+		BitmapFactory.Options newOpts = new BitmapFactory.Options();
+		// 开始读入图片，此时把options.inJustDecodeBounds 设回true了
+		newOpts.inJustDecodeBounds = true;
+		Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
+		newOpts.inJustDecodeBounds = false;
+		int w = newOpts.outWidth;
+		int h = newOpts.outHeight;
+		// 现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
+		float hh = 800f;// 这里设置高度为800f
+		float ww = 480f;// 这里设置宽度为480f
+		// 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+		int be = 1;// be=1表示不缩放
+		if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
+			be = (int) (newOpts.outWidth / ww);
+		} else if (w < h && h > hh) {// 如果高度高的话根据宽度固定大小缩放
+			be = (int) (newOpts.outHeight / hh);
+		}
+		if (be <= 0)
+			be = 1;
+		newOpts.inSampleSize = be;// 设置缩放比例
+		// 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+		isBm = new ByteArrayInputStream(baos.toByteArray());
+		bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
+		return compressImage(bitmap);// 压缩好比例大小后再进行质量压缩
+	}
+
+	/**
+	 * 质量压缩方法
+	 * 
+	 * @param image
+	 * @return
+	 */
+	public static Bitmap compressImage(Bitmap image) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		image.compress(Bitmap.CompressFormat.JPEG, 100, baos);// 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+		int options = 100;
+		while (baos.toByteArray().length / 1024 > 100) { // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
+			baos.reset();// 重置baos即清空baos
+			image.compress(Bitmap.CompressFormat.JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
+			options -= 10;// 每次都减少10
+		}
+		ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
+		Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
+		return bitmap;
+	}
+
+	/**
+	 * 读取图片属性：旋转的角度
+	 * 
+	 * @param path
+	 *            图片绝对路径
+	 * @return degree旋转的角度
+	 */
+	public static int readPictureDegree(String path) {
+		int degree = 0;
+		try {
+			ExifInterface exifInterface = new ExifInterface(path);
+			int orientation = exifInterface.getAttributeInt(
+					ExifInterface.TAG_ORIENTATION,
+					ExifInterface.ORIENTATION_NORMAL);
+			switch (orientation) {
+			case ExifInterface.ORIENTATION_ROTATE_90:
+				degree = 90;
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_180:
+				degree = 180;
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_270:
+				degree = 270;
+				break;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return degree;
+	}
+
+	/**
+	 * 旋转图片
+	 * 
+	 * @param angle
+	 * @param bitmap
+	 * @return Bitmap
+	 */
+	public static Bitmap rotaingImageView(int angle, Bitmap bitmap) {
+		// 旋转图片 动作
+		Matrix matrix = new Matrix();
+		matrix.postRotate(angle);
+		// 创建新的图片
+		Bitmap resizedBitmap = Bitmap.createBitmap(bitmap, 0, 0,
+				bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+		return resizedBitmap;
 	}
 }
